da1dbb776502ef7339007f400dc99e139ed0b0c0
Luca Bonavita <mindrones@users.noreply.github.com>
2018-07-23 09:55:35 +0100

    latest data

--- gulp/data.js
+++ gulp/data.js
@@ -1,16 +1,16 @@
 import gulp from "gulp";
 
-const COMPANIES_PATH = "data/companies.tsv";
+const COMPANIES_GLOB = "data/*";
 
 gulp.task("copy.data", done => {
-    gulp.src(COMPANIES_PATH)
+    gulp.src(COMPANIES_GLOB)
     .pipe(gulp.dest("build/data"));
 
     done();
 });
 
 gulp.task("watch.data", done => {
-    gulp.watch(COMPANIES_PATH, gulp.series("copy.data"));
+    gulp.watch(COMPANIES_GLOB, gulp.series("copy.data"));
 
     done();
 });
